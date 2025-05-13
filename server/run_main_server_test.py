0ef065f4d2bebb9ba7e144bd7836312c0c99c10c
j0hn_shi <270409498@qq.com>
2025-05-13 23:32:04 +0800

    update

---
+++ server/run_main_server_test.py
@@ -0,0 +1,4 @@
+from uplifted.server import run_main_server_internal
+
+if __name__ == "__main__":
+    run_main_server_internal()
---
+++ server/run_tool_server_test.py
@@ -0,0 +1,4 @@
+from uplifted.tools_server import run_tools_server_internal
+
+if __name__ == "__main__":
+    run_tools_server_internal()
